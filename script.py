63c546d29661c8b9083ef8b811dca94eed460c08
AtharvaKamble <atharvadkamble00@gmail.com>
2024-06-06 00:30:56 -0400

    add remove_prepend_data function

--- script.py
+++ script.py
@@ -21,10 +21,10 @@ def prepend_to_file(_dir):
         if '.md' not in file_name: continue
 
         f_path = f'{_dir}/{file_name}'
-        new_f_path = f'{_dir}/yeeh.md'
+
         title = file_name.replace('.md', '')
 
-        new_prepend_data = f'---\ntitle: {title}\ntags:\n\t- medium\n---'
+        new_prepend_data = f'---\ntitle: {title}\ntags:\n' + ' ' * 4 + '- medium\n---'
         file_cache = ''
 
         with open(f_path, encoding='utf-8') as f:
@@ -33,11 +33,41 @@ def prepend_to_file(_dir):
         with open(f_path, 'w', encoding='utf-8') as f:
             f.write(new_prepend_data + '\n' + file_cache)
         
+def remove_prepend_data(_dir):
+    names = os.listdir(_dir)
+
+    for file_name in names:
+        if '.md' not in file_name: continue
+
+        f_path = f'{_dir}/{file_name}'
+
+        title = file_name.replace('.md', '')
+
+        # confirm this data with the above function
+        # new_prepend_data = f'---\ntitle: {title}\ntags:\n    - medium\n---'
+        new_prepend_data = f'---\ntitle: {title}\ntags:\n' + ' ' * 4 + '- medium\n---'
+
+        file_cache = ''
+
+        with open(f_path, encoding='utf-8') as f:
+            file_cache += f.read()
+
+        file_cache = file_cache.replace(new_prepend_data, '')
+
+        with open(f_path, 'w', encoding='utf-8') as f:
+            f.write(file_cache)
         
 
 
 dir_you_want_to_clean = ''
 
-# clean_dir_name(dir_you_want_to_clean)
-prepend_to_file(dir_you_want_to_clean)
+# uncomment function to use
 
+# use to remove the last word in a file directory name
+# clean_dir_name(dir_you_want_to_clean)
+
+# prepend the file with the metadata for quartz 4 to recognize it as a valid markdown file
+# prepend_to_file(dir_you_want_to_clean)
+
+# remove the prepended data that was attached by the above function
+# remove_prepend_data(dir_you_want_to_clean)
